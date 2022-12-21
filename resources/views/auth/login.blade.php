640b72915e71ebdfb3cd17324a60eda9809776d1
judhacdc <jdxxaa@gmail.com>
2022-12-21 23:50:42 +0700

    SD

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -81,7 +81,8 @@
                 </a>
               </div>
               <!-- /Logo -->
-              <h4 class="mb-2">Welcome to kITabantuin! 👋</h4>
+              {{-- <h4 class="mb-2">Welcome to kITabantuin! 👋</h4> --}}
+              <h4 class="mb-2">TEST UBAH  👋</h4>
               <p class="mb-4">Please sign-in to your account and start the adventure</p>
 @if (count($errors) > 0)
 <div class="alert alert-danger">
